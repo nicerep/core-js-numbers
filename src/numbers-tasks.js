bf7ec0c0e3eabfdc3ee987c9788b7bb82eded6ea
yourName <your@email.xxx>
2023-12-22 15:02:01 +0300

    feat: complete getParallelepipedDiagonal task feat:  complete roundToPowerOfTen task feat: complete isPrime task feat: complete toNumber task feat: complete getCube task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -184,8 +184,9 @@ function parseNumberFromString(value) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  const parallelepipedDiagonal = Math.sqrt(c ** 2 + b ** 2 + a ** 2);
+  return parallelepipedDiagonal;
 }
 
 /**
@@ -205,8 +206,8 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  return Math.round(num / 10 ** pow) * 10 ** pow;
 }
 
 /**
@@ -226,8 +227,19 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  let isNumPrime = false;
+  if (n === 2) {
+    return true;
+  }
+  for (let i = 2; i <= Math.sqrt(n) + 1; i += 1) {
+    if (n % i < 1) {
+      isNumPrime = false;
+      break;
+    }
+    isNumPrime = true;
+  }
+  return isNumPrime;
 }
 
 /**
@@ -245,8 +257,11 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  if (!Number.isNaN(Number(value)) && typeof Number(value) === 'number') {
+    return Number(value);
+  }
+  return def;
 }
 
 /**
@@ -260,8 +275,8 @@ function toNumber(/* value, def */) {
  *   -2 => -8
  *   0  => 0
  */
-function getCube(/* num */) {
-  throw new Error('Not implemented');
+function getCube(num) {
+  return num ** 3;
 }
 
 /**
